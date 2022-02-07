ad7e314e5ed8772d201bfd06ed68b4dee8e6f586
ofirne <ofirne@avantisteam.com>
2022-02-07 09:55:36 +0200

    running model

--- graphics.py
+++ graphics.py
@@ -1,9 +1,15 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
-def plot_bacteria(bacteria_ndarray):
+def plot_bacteria(bacteria_ndarray: np.ndarray, dimensions: tuple, save_path: str = None, cmap: str='prism'):
+    im = bacteria_ndarray.reshape(dimensions)
+    fig = plt.imshow(im, cmap=cmap, vmin=0, vmax=1)
+    plt.title('Red/Green graphic distribution')
+    if save_path:
+        fig.write_png(save_path)
+
+def plot_green_TMG(green_history):
     pass
-
-def plot_green_TMG(history_ndarray):
     pass
 
 def create_gif(history_ndarray):
--- model.py
+++ model.py
@@ -5,7 +5,7 @@ TMG_CRITICAL_AMOUNT = 10**6
 
 
 def sigmoid(x):
-    return 1/(1 + np.exp(-x))
+    return 1/(1 + np.exp(-x)) - 0.5
 
 
 class TestMS:
@@ -16,13 +16,23 @@ class TestMS:
         self.GLU_amount = 0
         self.GLU_rate: float = GLU_rate
         self.p_to_double_TMG_amount: float = 0
-        self.history = []
+        self.bacteria_history = [self.bacteria]
+        self.green_history = []
+        self.TMG_history = []
+        self.save_history()
 
     def next_time_step(self):
         self.TMG_amount += self.TMG_rate
         self.GLU_amount += self.GLU_rate
-        self.history.append(self.bacteria)
+        turned_to_green = np.random.binomial(1, p=self.p_switch_color, size=self.bacteria.size)
+        self.bacteria =  self.bacteria | turned_to_green
+        self.save_history()
 
+    def save_history(self):
+        self.bacteria_history.append(self.bacteria)
+        self.green_history.append((self.bacteria.nonzero()[0].size))
+        self.TMG_history.append(self.TMG_amount)
+        
     @property
     def p_switch_color(self):
         return sigmoid(self.TMG_amount - self.GLU_amount)
